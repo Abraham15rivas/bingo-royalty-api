ff57ad42958ab52537fdf1333526e5f21fd8d8d9
Abraham15rivas <abraham15rivas@gmail.com>
2022-03-11 02:56:23 -0400

    logic for a cardboard

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\{
+    CardboardController
+};
 
+// Group route: v1.0 Bingo Royal
 Route::group([
-    'prefix' => 'v1',
+    'prefix' => 'v1.0',
 ], function () {
-
     Route::get('/users',function (Request $request){
         return User::all();
     });
@@ -16,11 +19,19 @@ Route::group([
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/signup', [AuthController::class, 'signup']);
 
+    // Group route: Authenticated users
     Route::group([
         'middleware' => 'auth:api',
     ], function () {
         Route::get('/logout', [AuthController::class, 'logout']);
         Route::get('/user',   [AuthController::class, 'user']);
+
+        // Group route: Admin
+        Route::group([
+            'prefix' => 'admin',
+            // 'middleware' => 'admin',
+        ], function () {
+            Route::get('cardboard', [CardboardController::class, 'index']);
+        });
     });
 });
-
