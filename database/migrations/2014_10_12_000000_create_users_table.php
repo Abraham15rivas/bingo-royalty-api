0266b5136f6bc3e422f5c55ec763e8eee69e712d
Diego <diego.rojas0640@gmail.com>
2022-05-15 21:03:14 -0400

    accounts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->boolean('vip')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
             $table->foreignId('role_id')
